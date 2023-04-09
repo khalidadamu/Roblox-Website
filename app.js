7249ae9203e0a3f1ccfaa1c97ebd6302968b2e15
Khalid Adamu <khalidbox720@gmail.com>
2023-04-09 20:41:57 +0100

    landing page

---
+++ app.js
@@ -0,0 +1,31 @@
+
+    const appData = () => {
+        return {
+            percent: 0,
+
+            appInit() {
+                window.addEventListener('scroll', () => {
+                    let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+                        height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+                    this.percent = Math.round((winScroll / height) * 100);
+                });
+            },
+        };
+    };
+
+
+    var swiper = new Swiper('.blog-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        mousewheel: {
+          invert: false,
+        },
+        // autoHeight: true,
+        pagination: {
+          el: '.blog-slider__pagination',
+          clickable: true,
+        }
+      });
+  
